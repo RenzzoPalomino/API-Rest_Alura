f41bbb90244031e954a6cf8a916dba26bcd5a84b
RenzzoPalomino <renzzoomar@gmail.com>
2023-10-01 17:48:05 -0500

    Agregando entidades de usuario y migracion

---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.usuarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor   //constructor sin atributos
+@AllArgsConstructor   //constructor con atributos
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String clave;
+}
